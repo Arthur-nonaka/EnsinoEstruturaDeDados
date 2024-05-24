9ec1aa87197657942591c5367560dede9b0d9c94
Arthur <nonakaodaarthur@gmail.com>
2024-05-24 17:58:35 -0300

    TAD

---
+++ TAD.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>W4School - TAD</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <main class="section">
+        <section class="section">
+            <h1 class="title has-text-info"> TAD </h1>
+            <h2 class="subtitle"> Tipo Abstrato de Dado </h2>
+        </section>
+        <section class="section">
+            <!-- <p class="has-text-justified is-size-3">
+            é uma
+            especificação de um conjunto de dados e operações
+            que podem ser executadas sobre esses dados.
+            </p> -->
+        </section>
+    </main>
+</body>
+</html>
