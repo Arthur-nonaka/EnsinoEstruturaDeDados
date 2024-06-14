7807c749b173eaf46ac30eb475963bec8668c063
Gustavo <gustavobispocosta5521@gmail.com>
2024-06-14 17:40:06 -0300

    Gustavo/css do index e do cadastro

--- index.php
+++ index.php
@@ -3,27 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <title>W4School - Login</title>
-    <link rel="stylesheet" href="style.css">
-    
+    <link rel="stylesheet" href="index.css">
 </head>
 <body class="corpo">
-    <background img src="img/fundo.gif" class="imgfundo">
-
     <section class="login">
         <form class="form" action="./funcoes/verificarCliente.php" method="POST">
             <h1 class="nome">Estrutura De Dados</h1>
-            
-                <input type="text" name="email" id="email" placeholder="E-mail">
-            
-                <input type="password" name="senha" id="senha" placeholder="Senha">
-           
-                <input type="submit" value="Entrar" class="botao">
-
-                <a href="cadastro.php" class="link">Criar Conta</a>
-        
+            <input type="text" name="email" id="email" placeholder="E-mail">
+            <input type="password" name="senha" id="senha" placeholder="Senha">
+            <input type="submit" value="Entrar" class="botao">
+            <a href="cadastro.php" class="link">Criar Conta</a>
         </form>
-
     </section>
-    
 </body>
 </html>
