26e9f7b41a9b3dc1b562e726ef38f0e9a1ae2989
Gustavo <gustavobispocosta5521@gmail.com>
2024-09-13 17:46:55 -0300

    Fila Fifo

--- componentes/sidebar.php
+++ componentes/sidebar.php
@@ -14,6 +14,8 @@
         <a href="/ensinoestruturadedados/explicacao/ListaDupla/Lista.php" class="sidebar-item">Lista Duplamente Encadeada</a>
         <a href="/ensinoestruturadedados/explicacao/ListaDupla/Operacoes.php" class="sidebar-subitem">Operações</a>
         <a href="/ensinoestruturadedados/explicacao/ListaDupla/Exemplo.php" class="sidebar-subitem">Exemplo</a>
+        <a href="/ensinoestruturadedados/explicacao/FilaFIFO/Exemplo.php" class="sidebar-item">Fila FIFO</a>
+        <a href="/ensinoestruturadedados/explicacao/FilaFIFO/Exemplo.php" class="sidebar-subitem">Operações</a>
     </div>
 </div>
 
