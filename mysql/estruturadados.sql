17b9502afb5d45ae1c17300ad78c5bd5ebb5df25
Arthur <nonakaodaarthur@gmail.com>
2024-09-27 15:10:21 -0300

    banco update

--- mysql/estruturadados.sql
+++ mysql/estruturadados.sql
@@ -5,3 +5,19 @@ CREATE TABLE users (
     email VARCHAR(255) NOT NULL,
     coins int(11) NOT NULL
 );
+
+CREATE TABLE 'users_items' (
+    users_id INT,
+    items_id INT,
+    PRIMARY KEY (users_id, items_id),
+    FOREIGN KEY (users_id) REFERENCES users(id),
+    FOREIGN KEY (items_id) REFERENCES items(id),
+
+)
+
+CREATE TABLE items (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    price FLOAT NOT NULL,
+    name varchar(40) NOT NULL,
+    path varchar(255) NOT NULL,
+)
