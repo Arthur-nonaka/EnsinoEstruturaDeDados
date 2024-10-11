4f81dc5440ad2fc701f2fadd57d42921c8398a3e
Arthur <nonakaodaarthur@gmail.com>
2024-10-11 12:46:27 -0300

    fix database

--- mysql/estruturadados.sql
+++ mysql/estruturadados.sql
@@ -1,23 +1,32 @@
 CREATE TABLE users (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    coins int(11) NOT NULL
+    coins INT(11) NOT NULL
 );
-
-CREATE TABLE 'users_items' (
-    users_id INT,
-    items_id INT,
-    PRIMARY KEY (users_id, items_id),
-    FOREIGN KEY (users_id) REFERENCES users(id),
-    FOREIGN KEY (items_id) REFERENCES items(id),
-
-)
 
 CREATE TABLE items (
     id INT PRIMARY KEY AUTO_INCREMENT,
     price FLOAT NOT NULL,
-    name varchar(40) NOT NULL,
-    path varchar(255) NOT NULL,
-)
+    name VARCHAR(40) NOT NULL,
+    path VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE users_items (
+    users_id INT,
+    items_id INT,
+    PRIMARY KEY (users_id, items_id),
+    FOREIGN KEY (users_id) REFERENCES users(id),
+    FOREIGN KEY (items_id) REFERENCES items(id)
+);
+
+CREATE TABLE questions (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    question VARCHAR(255) NOT NULL,
+    a VARCHAR(255) NOT NULL,
+    b VARCHAR(255) NOT NULL,
+    c VARCHAR(255) NOT NULL,
+    d VARCHAR(255) NOT NULL,
+    correct VARCHAR(1) NOT NULL
+);
